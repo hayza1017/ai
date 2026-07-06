6d85c0b6d9baef47a63c6cdae429f4857311bfba
hayza1017 <01039948354a@gmail.com>
2026-07-06 17:57:02 +0900

    모듈과패키지

---
+++ Source/pylib/sample_pac/ab/__init__.py
@@ -0,0 +1,9 @@
+# sample_pac/ab/__init__.py
+# sample_pac.ab 패키지를 import할 때 자동 로드 되는 파일
+# ab패키지 셋팅
+# ab밑에 a.py(공통적으로 많이 쓰는 함수)와 b.py(가끔 사용하는 함수)
+# from sample_pac.ab imoprt * 이렇게 쓸수 있도록 셋팅
+__all__ = ['a']
+
+print("sample_pac안의 ab패키지 로드됨")
+
